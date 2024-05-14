f44ee44b50af51988042b39ef0fb9f416324ea42
Ivan Robles <irae45@gmail.com>
2024-05-14 16:07:38 -0600

    add detail page styles adjustments.

--- src/components/DetailPage.jsx
+++ src/components/DetailPage.jsx
@@ -46,13 +46,12 @@ function PokeDetail() {
   } = data;
 
   return (
-    <div className="flex absolute top-0 left-0 justify-center items-center w-screen h-screen">
-
-      <div className="container relative bg-slate-100 z-10 w-full flex justify-evenly rounded-lg">
-        <div className="image">
+    <div className="flex justify-center">
+      <div className="py-4 relative bg-slate-100 z-10 w-full flex flex-col sm:flex-row justify-center items-center sm:justify-evenly rounded-lg">
+        <div className="image sm:w-1/4 flex justify-center sm:block">
           <img className="z-10" src={sprites.other.home.front_default} alt={name} height={256} width={256} />
         </div>
-        <div className="details text-left">
+        <div className="details text-left w-1/2 sm:w-1/5 ">
           <Link to="..">
             <button
               className="bg-red-500 z-10 px-2 py-1 rounded-sm absolute left-4 top-4"
@@ -62,7 +61,7 @@ function PokeDetail() {
             </button>
           </Link>
           <button
-            className="bg-yellow-400 z-10 px-2 py-1 rounded-sm absolute left-4 bottom-4"
+            className="bg-yellow-400 z-10 px-2 py-1 rounded-sm absolute right-4 top-4"
             onClick={() => {
               setIsFav(!isFav);
               if (!isFav) {
@@ -78,22 +77,34 @@ function PokeDetail() {
           <div className="sizes">
             <h3 className="text-2xl text-blue-400 font-mono font-extrabold">Sizes:</h3>
             <div>
-              {`Height: ${height}`}
+              <span className="font-bold">Height:</span>
+              {` ${parseFloat(height * 0.1).toFixed(2)} M`}
             </div>
             <div>
-              {`Weight: ${weight}`}
+              <span className="font-bold">Weight:</span>
+              {` ${parseFloat(weight * 0.1).toFixed(2)} KG`}
             </div>
           </div>
           <div className="abilities">
             <h3 className="text-2xl text-blue-400 font-mono font-extrabold">Abilities:</h3>
             {
-              abilities.map(({ ability }) => <div key={ability.name}>{ability.name}</div>)
+              abilities.map(({ ability }) => <div className="capitalize" key={ability.name}>{ability.name}</div>)
             }
           </div>
           <div className="stats">
             <h3 className="text-2xl text-blue-400 font-mono font-extrabold">Stats:</h3>
             {
-              stats.map(({ stat, base_stat: baseStat }) => <div key={stat.name}>{`${stat.name}: ${baseStat}`}</div>)
+              stats.map(({ stat, base_stat: baseStat }) => (
+                <div className="capitalize flex justify-between" key={stat.name}>
+                  <div className="font-extrabold">
+                    {stat.name}
+                    :
+                  </div>
+                  <div>
+                    {baseStat}
+                  </div>
+                </div>
+              ))
             }
           </div>
         </div>
